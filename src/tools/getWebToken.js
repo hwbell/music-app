20c604bf7cf1b3cda313799f6c13593db893407c
Harry <harold.william.bell@gmail.com>
2019-04-30 18:29:37 -0600

    add conditional for env or process.env var for apple key

--- src/tools/getWebToken.js
+++ src/tools/getWebToken.js
@@ -2,8 +2,14 @@ const jwt = require("jsonwebtoken");
 
 // the private stuff
 
-const { privateKeyId } = require('../env');
-// console.log('key: ' + privateKeyId)
+// const { privateKeyId } = require('../env');
+
+if ( process.env.REACT_APP_PRIVATE_KEY_ID ) {
+  const privateKeyId = process.env.REACT_APP_PRIVATE_KEY_ID;
+} else {
+  const privateKeyId = require('../env');
+}
+
 const teamId = process.env.REACT_APP_TEAM_ID;
 const keyId = process.env.REACT_APP_KEY_ID;
 
